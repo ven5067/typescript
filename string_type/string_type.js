33f653e7470911c6bf0dd020aee7ad2904c57bee
venugopal meesala <venugopal_meesala@gap.com>
2017-11-08 22:15:45 -0800

    String Type

---
+++ string_type/string_type.js
@@ -0,0 +1,13 @@
+"use strict";
+/* problem */
+var stringA;
+stringA = "Hello"; // assignment of string
+console.log(stringA);
+stringA = 30; // assignment of number
+console.log(stringA);
+/* solution */
+var stringB;
+stringB = "Hello";
+console.log(stringB);
+// stringB = 30; // Type '30' is not assignable to type 'string'
+console.log(stringB);
---
+++ string_type/string_type.ts
@@ -0,0 +1,15 @@
+/* problem */
+let stringA;
+stringA = "Hello"; // assignment of string
+console.log(stringA);
+stringA = 30; // assignment of number
+console.log(stringA);
+
+
+/* solution */
+let stringB: string;
+stringB = "Hello";
+console.log(stringB);
+// stringB = 30; // Type '30' is not assignable to type 'string'
+console.log(stringB);
+
